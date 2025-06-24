fc93b1cdef929b9b08bf2191be81cbe033a8cda3
Nayaravis <navis.ochieng@student.moringaschool.com>
2025-06-24 19:28:40 +0300

    - implemented fetching errors from json-server - create error card function

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const errorPreviewContainer = document.getElementById("error-preview");
+const errorList = document.getElementById("error-list");
 
 function displayEmpty() {
     errorPreviewContainer.innerHTML =  `
@@ -8,4 +9,38 @@ function displayEmpty() {
     `;
 };
 
-displayEmpty();
+function createErrorCard(errorObject) {
+    return `
+    <li class="p-3.5 w-full border-b-2 border-b-gray-400">
+        <span class="p-1.5 text-sm font-semibold 
+        ${
+            errorObject.status === "solved"
+            ? "bg-yellow-500"
+            : "bg-pink-500"
+        }
+        ">
+        ${errorObject.status.toUpperCase()}
+        </span>
+        <p class="text-white py-1.5 text-xl truncate">
+        ${errorObject.title}
+        </p>
+        <p class="text-gray-300">
+        ${errorObject.project}
+        </p>
+    </li>
+    `;
+};
+
+function getErrors() {
+    fetch("http://localhost:3000/errors")
+        .then(res => res.json())
+        .then(errors => errors.map(error => {
+            errorList.innerHTML += createErrorCard(error);
+        }).join(""));
+};
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    displayEmpty();
+    getErrors();
+});
